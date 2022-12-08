5d78140a64a8352494039026609d21318fe96614
djumanov <diyorbekjumanov1120@gmail.com>
2022-12-08 18:52:25 +0500

    added task

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-# babyloniam method
-
-# The variables are S and d given
+def main(S, d):
+    '''create a babylonian function.
+    
+    Args:
+        S (int): number
+        d (int): numnber
+        
+    Returns:
+        float: result
+    '''
+    return
