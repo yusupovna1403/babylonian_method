6703271ea1addeb1de62e5c36a71a20f4e0d2ffe
yusupovna1403 <yusupovna2003girl@gmail.com>
2022-12-20 20:48:50 +0500

    babylonian function

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-def main(S, d):
-    '''create a babylonian function.
+
+    #'''create a babylonian function.
     
-    Args:
-        S (int): number
-        d (int): numnber
+    #Args:
+        #S (int): number
+        #d (int): numnber
         
-    Returns:
-        float: result
-    '''
-    return
+    #Returns:
+        #float: result
+    #'''
+def main(S, d):
+    a = (S - d ** 2 ) / (2 * d)
+    b = a + d
+    x = b - a ** 2 / (2 * b)
+    return x
+S = 10
+d = 6
+print(main(S,d))
